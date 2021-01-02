1098719fe9b648ed854f2871e68df50be8ce3b16
renuvishnoi1 <renu@ranoliaventures.com>
2021-01-02 22:47:25 +0530

    test

--- app/Http/Controllers/UserApiController.php
+++ app/Http/Controllers/UserApiController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\Users;
 use Validator;
 use Str;
+use Hash;
 
 class UserApiController extends Controller
 {
@@ -40,18 +41,43 @@ class UserApiController extends Controller
         "data" =>  $user
         ],200);
     }
-    public function login(Request $request){
-    	if(Auth::attempt(['email'=>$request->email,'password'=>$request->password])){
-           $user = Auth::Users();
-           $reponseArray=[];
-       $reponseArray['token']=Str::random(10);
-       $reponseArray['name']=$user->name;
-       return response()->json($reponseArray,200);
-    	}else{
-    		 return response()->json(['error'=>'Unauthorized'],203);
-    	}
+    // public function login(Request $request){
+    //  // dd($request);
+    //     $data= Users::where(['email'=>$request->email,'password'=>$request->password])->get();
 
+    //    dd($data);
+    // 	if(count($data)){
+           
+    //    return response()->json($data,200);
+    // 	}else{
+    // 		 return response()->json(['error'=>'Unauthorized'],203);
+    // 	}
+
+    // }
+    public function login (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'email' => 'required|string|email|max:255',
+        'password' => 'required',
+    ]);
+    if ($validator->fails())
+    {
+        return response(['errors'=>$validator->errors()->all()], 422);
     }
+    $user = Users::where('email', $request->email)->first();
+    if ($user) {
+        if (Hash::check($request->password, $user->password)) {
+            $token = $user->token;
+            $response = ['token' => $token];
+            return response($response, 200);
+        } else {
+            $response = ["message" => "Password mismatch"];
+            return response($response, 422);
+        }
+    } else {
+        $response = ["message" =>'User does not exist'];
+        return response($response, 422);
+    }
+}
     public function categoryList(Request $request){
     	
 
